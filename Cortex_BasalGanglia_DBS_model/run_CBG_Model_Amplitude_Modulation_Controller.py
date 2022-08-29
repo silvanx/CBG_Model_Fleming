6b7b7c2fe56983a6c5b54c3913d9554535132c00
Kuba Orłowski <j.orlowski@student.uw.edu.pl>
2022-08-29 11:22:12 +0100

    Don't save spike times in amplitude modulation (reproducibility)

--- Cortex_BasalGanglia_DBS_model/run_CBG_Model_Amplitude_Modulation_Controller.py
+++ Cortex_BasalGanglia_DBS_model/run_CBG_Model_Amplitude_Modulation_Controller.py
@@ -41,39 +41,6 @@ import Global_Variables as GV
 h = neuron.h
 
 
-# def generate_poisson_spike_times(pop_size, start_time, duration, fr, timestep, random_seed):
-#     """ generate_population_spike_times generates (N = pop_size) poisson distributed spiketrains
-#         with firing rate fr.
-#
-#     Example inputs:
-#         pop_size = 10
-#         start_time = 0.0        # ms
-#         end_time = 6000.0       # ms
-#         timestep = 1            # ms
-#         fr = 1                  # Hz
-#     """
-#
-#     # Convert to sec for calculating the spikes matrix
-#     dt = float(timestep) / 1000.0  # sec
-#     tSim = float(((start_time + duration) - start_time) / 1000.0)  # sec
-#     nBins = int(np.floor(tSim / dt))
-#
-#     spikeMat = np.where(np.random.uniform(0, 1, (pop_size, nBins)) < fr * dt)
-#
-#     # Create time vector - ms
-#     tVec = np.arange(start_time, start_time + duration, timestep)
-#
-#     # Make array of spike times
-#     for neuron_index in np.arange(pop_size):
-#         neuron_spike_times = tVec[spikeMat[1][np.where(spikeMat[0][:] == neuron_index)]]
-#         if neuron_index == 0:
-#             spike_times = Sequence(neuron_spike_times)
-#         else:
-#             spike_times = np.vstack((spike_times, Sequence(neuron_spike_times)))
-#
-#     return spike_times
-
-
 def generate_DBS_Signal(start_time, stop_time, dt, amplitude, frequency, pulse_width, offset):
     """Generate monophasic square-wave DBS signal
 
@@ -106,38 +73,12 @@ def generate_DBS_Signal(start_time, stop_time, dt, amplitude, frequency, pulse_w
     return DBS_Signal, times, next_pulse_time
 
 
-# def make_beta_cheby1_filter(Fs, N, rp, low, high):
-#     """Calculate bandpass filter coefficients (1st Order Chebyshev Filter)"""
-#     nyq = 0.5 * Fs
-#     lowcut = low / nyq
-#     highcut = high / nyq
-#
-#     b, a = signal.cheby1(N, rp, [lowcut, highcut], 'band')
-#
-#     return b, a
-
-
-# def calculate_avg_beta_power(lfp_signal, tail_length, beta_b, beta_a):
-#     """Calculate the average power in the beta-band for the current LFP signal window, i.e. beta Average Rectified Value (ARV)
-#
-# 	Exaqmple inputs:
-# 		lfp_signal 			- window of LFP signal												# samples
-# 		tail_length 		- tail length which will be discarded due to filtering artifact		# samples
-# 		beta_b, beta_a 		- filter coefficients for filtering the beta-band from the signal
-# 	"""
-#
-#     lfp_beta_signal = signal.filtfilt(beta_b, beta_a, lfp_signal)
-#     lfp_beta_signal_rectified = np.absolute(lfp_beta_signal)
-#     avg_beta_power = np.mean(lfp_beta_signal_rectified[-2 * tail_length:-tail_length])
-#
-#     return avg_beta_power
-
-
 if __name__ == '__main__':
 
     # Setup simulation
     setup(timestep=0.01, rngseed=3695)
     steady_state_duration = 6000.0  # Duration of simulation steady state
+    # TODO: Fix the simulation error when simulation runtime < 5999.0
     simulation_runtime = 32000.0  # Duration of simulation from steady state
     simulation_duration = steady_state_duration + simulation_runtime + simulator.state.dt  # Total simulation time
     rec_sampling_interval = 0.5  # Signals are sampled every 0.5 ms
@@ -164,11 +105,11 @@ if __name__ == '__main__':
     STN_space = space.RandomStructure(boundary=space.Sphere(2000))  # Sphere with radius 2000um
 
     # Generate Possoin-distributed Striatal Spike trains
-    striatal_spike_times = generate_poisson_spike_times(Pop_size, steady_state_duration, simulation_runtime, 20, 1.0,
-                                                        3695)
+    # striatal_spike_times = generate_poisson_spike_times(Pop_size, steady_state_duration, simulation_runtime, 20, 1.0,
+    #                                                     3695)
 
     # Save/load Striatal Spike times
-    np.save('Striatal_Spike_Times.npy', striatal_spike_times)  # Save spike times so they can be reloaded
+    # np.save('Striatal_Spike_Times.npy', striatal_spike_times)  # Save spike times so they can be reloaded
     striatal_spike_times = np.load('Striatal_Spike_Times.npy', allow_pickle=True)  # Load spike times from file
     for i in range(0, Pop_size):
         spike_times = striatal_spike_times[i][0].value
