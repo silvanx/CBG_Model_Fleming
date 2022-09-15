d839d41eeaeaddf67057e9dc0f3d3cbb3552e115
Kuba Orłowski <j.orlowski@student.uw.edu.pl>
2022-09-15 11:14:43 +0100

    Update plotting results to plot multiple files in a single plot and add type hints in plot_utils

--- Cortex_BasalGanglia_DBS_model/Simulation_Output_Results/plot_utils.py
+++ Cortex_BasalGanglia_DBS_model/Simulation_Output_Results/plot_utils.py
@@ -3,15 +3,19 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 from pathlib import Path
+from typing import Union, Any
+from numpy.typing import NDArray
 
 
-def mat_to_dict(obj):
+def mat_to_dict(obj: Any) -> dict:
+    '''Reads a matlab struct and turns it into a dictionary'''
     return dict(zip((e[0] for e in obj.dtype.descr), obj))
 
 
-def load_dbs_output(dir):
+def load_dbs_output(dir: Path) -> tuple[dict, dict]:
+    '''Reads DBS current data from a .mat file'''
     dbs_file = sio.loadmat(dir / 'DBS_Signal.mat')
-    segments, name, _ = dbs_file['block'][0, 0]
+    segments, _, _ = dbs_file['block'][0, 0]
     segment = mat_to_dict(segments[0, 0][0, 0])
     signal0, signal1 = segment['analogsignals'][0]
     dbs = mat_to_dict(signal0[0, 0])
@@ -20,16 +24,20 @@ def load_dbs_output(dir):
     return time, dbs
 
 
-def load_stn_lfp(dir, steady_state_time, sim_time):
+def load_stn_lfp(dir: Path, steady_state_time: float, sim_time: float)\
+        -> tuple[NDArray[np.float32], dict]:
+    '''Reads STN LFP data from a .mat file'''
     lfp_file = sio.loadmat(dir / 'STN_LFP.mat')
-    segments, name, _ = lfp_file['block'][0, 0]
+    segments, _, _ = lfp_file['block'][0, 0]
     segment = mat_to_dict(segments[0, 0][0, 0])
     lfp = mat_to_dict(segment['analogsignals'][0, 0][0, 0])
     lfp_t = np.linspace(steady_state_time, sim_time, len(lfp['signal']))
     return lfp_t, lfp
 
 
-def load_controller_data(dir, parameter):
+def load_controller_data(dir: Path, parameter: str)\
+        -> tuple[NDArray[np.float32], NDArray[np.float32]]:
+    '''Reads controller data from CSV files'''
     with open(dir / 'controller_sample_times.csv', 'r') as f:
         controller_t = np.array([float(r[0]) for r in csv.reader(f)])
     with open(dir / ('controller_%s_values.csv' % parameter), 'r') as f:
@@ -37,14 +45,18 @@ def load_controller_data(dir, parameter):
     return controller_t, controller_p
 
 
-def time_to_sample(tt, t):
+def time_to_sample(tt: np.ndarray, t: float) -> int:
+    '''Finds the first index in a time array where time is greater than or
+    equal to t.'''
     return np.where(tt >= t)[0][0]
 
 
-def plot_controller_result(plot_start_t, plot_end_t, parameter,
-                           time, dbs, controller_t, controller_p,
-                           lfp_time, lfp):
-    fig, axs = plt.subplots(3, 1, figsize=(15, 8))
+def plot_controller_result(plot_start_t: float, plot_end_t: float,
+                           parameter: str, time: dict, dbs: dict,
+                           controller_t: np.ndarray, controller_p: np.ndarray,
+                           lfp_time, lfp, axs: list[plt.Axes] = None):
+    if axs is None:
+        fig, axs = plt.subplots(3, 1, figsize=(15, 8))
     s = time_to_sample(time['signal'], plot_start_t)
     e = time_to_sample(time['signal'], plot_end_t) - 1
     axs[0].plot(time['signal'][s:e], dbs['signal'][s: e])
@@ -64,14 +76,24 @@ def plot_controller_result(plot_start_t, plot_end_t, parameter,
     axs[2].set_ylabel('Local field potential [%s]' % lfp['signal_units'][0])
     axs[2].set_xlabel('Time [ms]')
     plt.subplots_adjust(hspace=0.3)
+    return axs
 
 
-def load_and_plot(dirname, parameter, steady_state_time, sim_time,
-                  plot_start_t, plot_end_t):
-    directory = Path(dirname)
-    time, dbs = load_dbs_output(directory)
-    lfp_time, lfp = load_stn_lfp(directory, steady_state_time, sim_time)
-    controller_t, controller_p = load_controller_data(directory, parameter)
-    plot_controller_result(plot_start_t, plot_end_t, parameter,
-                           time, dbs, controller_t, controller_p,
-                           lfp_time, lfp)
+def load_and_plot(dirname: Union[str, list[str]], parameter: str,
+                  steady_state_time: float, sim_time: float,
+                  plot_start_t: float, plot_end_t: float) -> None:
+    '''Loads and plots the simulation result from a given directory.
+
+    If dirname is a list of strings, displays all the results in one plot.
+    '''
+    if isinstance(dirname, str):
+        dirname = [dirname]
+    axs = None
+    for d in dirname:
+        directory = Path(d)
+        time, dbs = load_dbs_output(directory)
+        lfp_time, lfp = load_stn_lfp(directory, steady_state_time, sim_time)
+        controller_t, controller_p = load_controller_data(directory, parameter)
+        axs = plot_controller_result(plot_start_t, plot_end_t, parameter, time,
+                                     dbs, controller_t, controller_p, lfp_time,
+                                     lfp, axs)
